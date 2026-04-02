17298d3c49679f664637e02d02b9a57006551f87
kapoko <17455539+kapoko@users.noreply.github.com>
2026-04-02 12:34:58 +0200

    Log error when things go south

--- importer/run-sync.sh
+++ importer/run-sync.sh
@@ -12,7 +12,13 @@ cleanup() {
   rmdir "$lockdir" 2>/dev/null || true
 }
 
-trap cleanup EXIT INT TERM
+on_interrupt() {
+  echo "<3> Email backup interrupted" >&2
+  exit 2
+}
+
+trap cleanup EXIT
+trap on_interrupt INT TERM
 
 echo "Starting Gmail import"
 python3 /usr/local/bin/gmail-import.py
